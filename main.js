78f2af84fcf63b7bfc0a230e2381139f9887c5c0
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-10-01 10:57:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var SpeechRecognition=window.webkitSpeechRecognition;
 var recognition= new SpeechRecognition();
+var Content;
 
 function start(){
 document.getElementById("textbox").innerHTML="";
 recognition.start();
-
 }
+
 recognition.onresult=function(event){
 console.log(event);
 var Content=event.results[0][0].transcript;
@@ -23,15 +24,36 @@ speak();
 function speak(){
 var synth=window.speechSynthesis;
 speak_data="Taking selfie in 5 seconds";
+Webcam.attach(camera);
 
+setTimeout(function(){
+    img_id="selfie_1";
+
+take_snapshot();
+speak_data="taking your selfie in 5 secords";
 var utterThis=new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
-Webcam.attach(camera);
-setTimeout(function(){
-take_snapshot();
-save();
 },5000);
+setTimeout(function(){
+    img_id="selfie_2";
+
+take_snapshot();
+speak_data="taking your selfie in 10 secords";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+},10000);
+setTimeout(function(){
+    img_id="selfie_3";
+
+take_snapshot();
+speak_data="taking your selfie in 15 secords";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+},15000);
+
+
 } 
+camera=document.getElementById("camera");
 Webcam.set({
 width:360,
 height: 250,
@@ -39,12 +61,19 @@ image_format: 'png',
 png_quality:90
 }
 );
-camera=document.getElementById("camera");
+
 
 function take_snapshot(){
 Webcam.snap(function(data_uri){
-document.getElementById("result").innerHTML='<img id="selfie_img" src="'+ data_uri+'">';
-
+if (img_id=="selfie_1") {
+    document.getElementById("result1").innerHTML='<img id="selfie_1" src="'+ data_uri+'">';    
+}
+if (img_id=="selfie_2") {
+    document.getElementById("result2").innerHTML='<img id="selfie_2" src="'+ data_uri+'">';    
+}
+if (img_id=="selfie_3") {
+    document.getElementById("result3").innerHTML='<img id="selfie_3" src="'+ data_uri+'">';    
+}
 
 });
 }
